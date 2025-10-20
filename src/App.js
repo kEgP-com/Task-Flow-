008da0a9a0000dd8b487a71d2c156ec0a8b890d5
Rere-arch <racheljoyramos9@gmail.com>
2025-10-20 13:28:21 +0800

    Ramos

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import NavigationBar from './components/Navbar';
 import TaskListView from './pages/TaskListView';
 import AddTaskView from './pages/AddTaskView';
 
-// Ramos
+// Ramos, Rachel 
 function App() {
   const [tasks, setTasks] = useState([]);
 
